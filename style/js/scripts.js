14c10ae6e989c8376eb2ba438ffaaff0b25aff14
KEVIN BAZIRA <kvnbzr@gmail.com>
2018-04-29 18:12:14 +0300

    v 1.0.8

--- style/js/scripts.js
+++ style/js/scripts.js
@@ -51,43 +51,7 @@ $(document).ready(function () {
     
 
 });
-/*-----------------------------------------------------------------------------------*/
-/*	FANCYBOX
-/*-----------------------------------------------------------------------------------*/
-$(document).ready(function () {
-    $(".fancybox-media").fancybox({
-        arrows: true,
-        padding: 0,
-        closeBtn: true,
-        openEffect: 'fade',
-        closeEffect: 'fade',
-        prevEffect: 'fade',
-        nextEffect: 'fade',
-        helpers: {
-            media: {},
-            overlay: {
-                locked: false
-            },
-            buttons: false,
-            thumbs: {
-                width: 50,
-                height: 50
-            },
-            title: {
-                type: 'inside'
-            }
-        },
-        beforeLoad: function () {
-            var el, id = $(this.element).data('title-id');
-            if (id) {
-                el = $('#' + id);
-                if (el.length) {
-                    this.title = el.html();
-                }
-            }
-        }
-    });
-});
+
 /*-----------------------------------------------------------------------------------*/
 /*	TABS
 /*-----------------------------------------------------------------------------------*/
@@ -296,23 +260,7 @@ $(document).ready(function () {
 $(document).ready(function () {
     $('.icon-overlay a').prepend('<span class="icn-more"></span>'); /* '<i class="fa fa-link"></i>' */
 });
-/*-----------------------------------------------------------------------------------*/
-/*	HOME SLIDER
-/*-----------------------------------------------------------------------------------*/
-$(document).ready(function () {
-    var revapi;
-    jQuery(document).ready(function () {
 
-        revapi = jQuery('.fullwidthbanner').revolution({
-            delay: 9000,
-            startwidth: 1170,
-            startheight: 600,
-            hideThumbs: 200,
-            fullWidth: "on"
-        });
-
-    });
-});
 /*-----------------------------------------------------------------------------------*/
 /*	PRETTIFY
 /*-----------------------------------------------------------------------------------*/
