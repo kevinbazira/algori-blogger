999df39c44761cb9ae77d406aa6b90dfc7552d80
KEVIN BAZIRA <kvnbzr@gmail.com>
2019-04-15 14:54:53 +0300

    Added: documentation link to customizer
    Added Algori Blogger documentation link to customizer

--- inc/customizer.php
+++ inc/customizer.php
@@ -5,6 +5,8 @@
  * @package Algori_Blogger
  */
 
+ 
+ 
 /**
  * Add postMessage support for site title and description for the Theme Customizer.
  *
@@ -25,6 +27,46 @@ function algori_blogger_customize_register( $wp_customize ) {
 			'render_callback' => 'algori_blogger_customize_partial_blogdescription',
 		) );
 	}
+	
+	/**
+	* Algori Blogger Custom Customizer Customizations [ PS: The order matters ( add_panel, add_section, add_setting, add_control ) ]
+	*/
+	
+	/**
+	 * Create custom control for Algori Blogger Documentation link.
+	 *
+	 * Class WP_Customize_Control is loaded only when theme customizer is acutally used. 
+	 * So, I've defined my custom class within the algori_blogger_customize_register function that is binded to the 'customize_register' action.
+	 *
+	 * @link https://developer.wordpress.org/themes/customize-api/customizer-objects/#custom-controls-sections-and-panels.
+	 */ 
+	class WP_Documentation_Link_Customize_Control extends WP_Customize_Control {
+	  public $type = 'algori_blogger_documentation_link';
+	  /** 
+	  * Render the control's content. esc_url($my_theme->get( 'AuthorURI' ))
+	  */
+	  public function render_content() {
+	  ?>
+		<a href="<?php echo esc_url( wp_get_theme()->get( 'AuthorURI' ) ) . 'blog/article/algori-blogger-documentation'; ?>" class="button button-primary" id="algori-blogger-documentation-link" target="_blank" tabindex="0"><?php _e( 'Documentation', 'algori-blogger' ); ?></a>
+	  <?php
+	  }
+	}
+	
+	// Documentation Section
+	$wp_customize->add_section( 'algori_blogger_documentation', array(
+		'title'				=> __( 'Algori Blogger Documentation', 'algori-blogger' ),
+		'description'		=> __( 'View Algori Blogger Documentation.', 'algori-blogger' ),
+		'priority'			=> 1,
+		'capability'		=> 'edit_theme_options',
+	    'theme_supports' 	=> '', // Rarely needed.
+	) );
+	
+	// Documentation Button ( from custom control WP_Documentation_Link_Customize_Control )
+	$wp_customize->add_control( new WP_Documentation_Link_Customize_Control( $wp_customize, 'documentation_button', array(
+        'section'			=> 'algori_blogger_documentation',
+        'settings'			=> array(),
+    ) ) );
+	
 }
 add_action( 'customize_register', 'algori_blogger_customize_register' );
 
@@ -50,6 +92,14 @@ function algori_blogger_customize_partial_blogdescription() {
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function algori_blogger_customize_preview_js() {
-	wp_enqueue_script( 'algori-blogger-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+	wp_enqueue_script( 'algori-blogger-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20190415', true );
 }
 add_action( 'customize_preview_init', 'algori_blogger_customize_preview_js' );
+
+/**
+ * Enqueue CSS for styling custom controls in the customizer.
+ */
+function algori_blogger_customize_preview_assets() {
+	wp_enqueue_style( 'algori-blogger-customizer-custom-controls', get_template_directory_uri() . '/style/css/customizer.css', array(), '20190415', 'all' );
+}
+add_action( 'customize_controls_enqueue_scripts', 'algori_blogger_customize_preview_assets' );
